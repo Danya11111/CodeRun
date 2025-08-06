84574848b11519a0dbd7f9064dfae2d17c2449d2
danilka_arkhipov <daniil.arhipov.2005@gmail.com>
2025-08-06 22:52:48 +0300

    Added 1. Средний элемент

---
+++ CodeRun_tests/1. Средний элемент.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int prog1()
+{
+    int a[3];
+    for (int i = 0; i < 3; ++i) cin >> a[i];
+    sort(a, a + 3);
+    cout << a[1] << endl;
+    return 0;
+}
--- CodeRun_tests/main.cpp
+++ CodeRun_tests/main.cpp
@@ -1,9 +1,13 @@
+#include "1. Средний элемент.h"
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
 
-	cout << "Hello world!" << endl;
-
+	cout << "Start program" << endl;
+	prog1();
+	cout << "End program" << endl;
 	return 0;
 }
